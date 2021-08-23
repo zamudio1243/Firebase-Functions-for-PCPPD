b58569b4eea7d0a6f3dc85b01f59f4db634d14ab
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2021-08-23 13:46:51 -0500

    :sparkles: Se almacena información del usuario propietario en el espacio de trabajo

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-// import { Workspace } from './models/workspace';
-// import { User } from './models/user';
+import { Workspace } from './models/workspace';
+import { User } from './models/user';
 
 const db = admin.firestore();
 
@@ -10,23 +10,24 @@ export const defaultSettings = functions.firestore
     .onCreate(async(snapshot,context)=>{
  
         const workSpaceRef = db.doc(`espacios_de_trabajo/${context.params.id}`);
-        //const workSpaceData = <Workspace>(await workSpaceRef.get()).data();
+        const workSpaceData = <Workspace>(await workSpaceRef.get()).data();
         
         const channel = {
             "nombre":  "General",
             "permisos": []
         };
 
-        // const user = await db.collection("usuarios")
-        //     .where("uid", "==", workSpaceData.uid_usuario).get()
-        //     .then((querySnapshot) => {
-        //         querySnapshot.forEach((doc)=> {
-        //             <User> doc.data();
-        //         });
-        //     });
+        await db.collection("usuarios")
+            .where("uid", "==", workSpaceData.uid_usuario).get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc)=> {
+                   const user = <User> doc.data();
+                   workSpaceRef.collection("usuarios").add(user)
+                });
+            });
         
 
-        // workSpaceRef.collection("usuarios").add(user)
+        
         workSpaceRef.collection("canales_voz").add(channel)
         return workSpaceRef.collection("canales_texto").add(channel)
 
